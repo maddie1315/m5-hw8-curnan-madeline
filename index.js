655436517e91d0fbc8a253037dc6d0e83b6060ad
madeline curnan <madelinecurnan@ufl.edu>
2021-10-16 17:18:41 -0400

    Add random number

--- index.js
+++ index.js
@@ -19,22 +19,24 @@ var lossElement = document.getElementById('losses')
 // determine which box was clicked with 'this.textContent' or event.target.textContent
 // convert that value to a Number and store it to a variable
 var boxElements = document.querySelectorAll('.box')
+
 for (let i = 0; i < boxElements.length; i++) {
     var boxElement = boxElements[i];
     boxElement.onclick = function(event) {
-        var boxNumberWasClicked = event.target.textContent
+        var boxNumberWasClicked = event.target.textContent;
         console.log(parseInt(boxNumberWasClicked, 10))
     }
 }
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
-
+var winningNum = Math.floor((Math.random() * 3) + 1);
 
 // determine if the box clicked is equal to the random number
 // if the numbers match, display a winning message by changing the text content of the div#message element
 // if the numbers match, increment wins and display the win count in div#wins
 // if the numbers don't match, change the div#message element's text to a random losing message from the array above
 // if the numbers don't match, increment losses and display the loss count in div#losses
+
 
 
